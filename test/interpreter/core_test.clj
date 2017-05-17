fca2aa0c8ba317f7c3396ce0a9bfd15938d74fe4
Toni Haka-Risku <thakarisku@qvantel.com>
2017-05-17 22:09:38 +0300

    more cases to tokenizer test

--- test/interpreter/core_test.clj
+++ test/interpreter/core_test.clj
@@ -6,5 +6,8 @@
 (deftest test-tokenizer
   (is (= [{:type CHR :value "def"},
           {:type BLANK :value " "},
-          {:type INT :value "1"}] (tokenize (split "def 1" #"")))))
+          {:type INT :value "12"},
+          {:type CHR :value "test"},
+          {:type BLANK :value "  "}
+          ] (tokenize (split "def 12test  " #"")))))
 
